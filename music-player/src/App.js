2cd85197e5df8ba4c89d3c277945b3891668553b
nimaester <nimaester@gmail.com>
2021-01-10 23:42:05 -0800

    updated boilerplate code

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-    <h1>Music Player</h1>
+      <h1>Music Player</h1>
     </div>
   );
 }
