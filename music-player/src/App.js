9101589303bacf47b90a96e4e1cd23d7a13eb291
nimaester <nimaester@gmail.com>
2021-01-10 23:45:21 -0800

    updated boilerplate code

--- music-player/src/App.js
+++ music-player/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MusicPlayer from './MusicPlayer';
 
 const App = () => {
   return (
     <div className="App">
       <h1>Music Player</h1>
+      <MusicPlayer />
     </div>
   );
 }
---
+++ music-player/src/MusicPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MusicPlayer = () => {
+  return (
+    <div>
+      <h1>Player for the music</h1>
+    </div>
+  );
+};
+
+export default MusicPlayer;
