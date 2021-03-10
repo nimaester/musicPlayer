fdfb439b1b7f17dec52d97678f8d60aa03a8efcd
nimaester <nimaester@gmail.com>
2021-03-10 02:12:16 -0800

    fixed some styling issues

--- music-player/src/Nav.js
+++ music-player/src/Nav.js
@@ -2,18 +2,19 @@ import React from "react";
 
 const Nav = ({ setShowTracks, showTracks }) => {
   return (
-    <div>
-      <nav>
-      <h2>Music Player</h2>
-        <button className={showTracks ? "highlight" : ""}
+    <nav>
+      <div className='header'>
+        <h2>Music Player</h2>
+        <button
+          className={showTracks ? "highlight" : ""}
           onClick={() => {
             setShowTracks(!showTracks);
           }}
         >
-          Track List {' '}
+          Track List{" "}
         </button>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
